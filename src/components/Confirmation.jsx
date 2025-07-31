730f2166b430443a091918e27ce5c2e1bda0f8b2
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-07-31 11:44:27 +0800

    feat: basic logic for Confirmation component

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,16 +1,44 @@
 export function Confirmation( {genInfo, educInfo, profInfo} ){
-  function compiledInformation(){
+  function displayGenInfo(){
     return (
       <section>
         <h3>General Information</h3>
         <p>Name: {genInfo.name}</p>
+        <p>Email: {genInfo.email}</p>
+        <p>Contact Number: {genInfo.contactNumber}</p>
+      </section>
+    );
+  }
+
+  function displayEducInfo(){
+    return (
+      <section>
+        <h3>Educational Information</h3>
+        <p>School Name: {educInfo.school}</p>
+        <p>Course: {educInfo.course} </p>
+        <p>Year Graduated: {educInfo.graduateYear}</p>
+      </section>
+    );  
+  }
+
+  function displayProfInfo(){
+    return(
+      <section>
+        <h3>Professional Information</h3>
+        <p>Company: {profInfo.company}</p>
+        <p>Position: {profInfo.position}</p>
+        <p>Responsibilities: {profInfo.responsibilities}</p>
+        <p>Started working on year: {profInfo.yearWorkedFrom}</p>
+        <p>Stopped working on year: {profInfo.yearWorkedTo}</p>
       </section>
     );
   }
 
   return(
     <div>
-        {compiledInformation()}
+        {displayGenInfo()}
+        {displayEducInfo()}
+        {displayProfInfo()}
     </div>
     
   );
